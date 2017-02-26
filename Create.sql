e8f864987180dcd099a6a0d6fe4e67de7bbe1386
Arthur Aguiar Estigarribia <arthuraguiarestigarribia@hotmail.com>
2017-02-26 12:31:56 -0300

    Update Create.sql
    Adição da inserção das tabelas.

--- Create.sql
+++ Create.sql
@@ -1,1 +1,20 @@
 CREATE DATABASE usuarios;
+
+-- Criar tabelas no database usuarios
+CREATE TABLE IF NOT EXISTS usuarios(
+    id SERIAL NOT NULL PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100),
+    senha VARCHAR(32)
+);
+
+CREATE TABLE IF NOT EXISTS calculos(
+    id SERIAL NOT NULL PRIMARY KEY,
+    id_usuario INTEGER REFERENCES usuarios(id),
+    categoria VARCHAR(100),
+    dado1 VARCHAR(100),
+    dado2 VARCHAR(100),
+    dado3 VARCHAR(100),
+    dado4 VARCHAR(100),
+    resultados VARCHAR(1000)
+);
